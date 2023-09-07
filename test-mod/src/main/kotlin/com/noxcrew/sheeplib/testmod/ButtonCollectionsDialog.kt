1537f98b7f482542371b0c9774b16847dc0a1a4a
Lucy Poulton <lucy.poulton@noxcrew.com>
2023-09-07 23:04:28 +0100

    ButtonStacks too

--- test-mod/src/main/kotlin/com/noxcrew/sheeplib/testmod/ButtonCollectionsDialog.kt
+++ test-mod/src/main/kotlin/com/noxcrew/sheeplib/testmod/ButtonCollectionsDialog.kt
@@ -7,9 +7,12 @@ import com.noxcrew.sheeplib.layout.grid
 import com.noxcrew.sheeplib.layout.iconButtonRow
 import com.noxcrew.sheeplib.theme.Theme
 import com.noxcrew.sheeplib.theme.Themed
+import com.noxcrew.sheeplib.util.ComponentBuilder
+import com.noxcrew.sheeplib.widget.ButtonStack
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.layouts.Layout
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.TextColor
 
 /**
  * A dialog showcasing button collection builders.
@@ -17,8 +20,11 @@ import net.minecraft.network.chat.Component
 public class ButtonCollectionsDialog(x: Int, y: Int) : Dialog(x, y), Themed by Theme.Active {
 
     private companion object {
+
+        private val letters = listOf("A", "B", "C", "D", "E", "F")
+
         /** Adds a message to the chat window. */
-        fun pushChatMessage(message: String) {
+        private fun pushChatMessage(message: String) {
             Minecraft.getInstance().gui.chat.addMessage(Component.literal(message))
         }
     }
@@ -26,12 +32,19 @@ public class ButtonCollectionsDialog(x: Int, y: Int) : Dialog(x, y), Themed by T
     override val title: DialogTitleWidget = TextTitleWidget(this, Component.literal("Button Collections"))
 
     override fun layout(): Layout = grid {
-
-        // icon button rows display
-        iconButtonRow(this@ButtonCollectionsDialog) {
-            listOf("A", "B", "C", "D", "E", "F").forEach {
-                it literal { pushChatMessage("Button $it pressed") }
+        val buttonCount = iconButtonRow(this@ButtonCollectionsDialog) {
+            letters.forEach {
+                it literal { pushChatMessage("Icon button $it pressed") }
             }
         }
+        ButtonStack(this@ButtonCollectionsDialog, 100, Minecraft.getInstance().font.lineHeight + 2) {
+            letters.forEach {
+                ComponentBuilder.literal("Button ") {
+                    +ComponentBuilder.literal(it) {
+                        color = TextColor.fromRgb(0x00ff00)
+                    }
+                } runs { pushChatMessage("Text button $it pressed") }
+            }
+        }.atBottom(0, buttonCount)
     }
 }
